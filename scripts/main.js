653add787f97d5a83adc346585790d157211f3c6
ugcitem <138194312+ugcitem@users.noreply.github.com>
2023-08-31 11:54:00 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,17 +3,10 @@ document.addEventListener('DOMContentLoaded', function() {
   const viewButton = document.getElementById('viewButton');
 
   viewButton.addEventListener('click', function() {
-    let link = linkInput.value;
-    
-    if (link.startsWith('https://')) {
-      link = link.substring(8); // Remove 'https://'
-    } else if (link.startsWith('http://')) {
-      link = link.substring(7); // Remove 'http://'
-    }
-
+    const link = linkInput.value;
     if (link) {
       const newWindow = window.open('about:blank', '_blank');
-      newWindow.document.write(`<iframe src="https://${link}" style="width: 100%; height: 100vh; border: none;"></iframe>`);
+      newWindow.document.write(`<iframe src="${link}" style="width: 100%; height: 100vh; border: none;"></iframe>`);
     }
   });
 });
